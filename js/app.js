95cb64e5b4135a22d1cc090908af8e74065edc74
Osamamomani1 <momaniosama97@gmail.com>
2021-04-26 15:57:59 +0300

    what you know about me lab02

---
+++ js/app.js
@@ -0,0 +1,89 @@
+'user strict';
+
+alert('welcome to Osama Momani page') ;
+
+let Qus= prompt('what\'s your name ?');
+
+con= confirm('wlcome '+ Qus+' , lets see how mutch you know about me ') ;
+
+let Qus1= prompt('Do you know who I\'m ?');
+
+switch (Qus1.toLowerCase()) {
+
+    case 'yes':
+    case 'y' : 
+    alert('you get that')
+break;
+    case 'no':
+    case 'n' : 
+    alert ('runge answer')
+break;
+default:
+    alert('invalid answer')
+}
+
+let Qus2= prompt('Do you think I\'m a gamer ?') ;
+
+switch (Qus2.toLowerCase()) {
+
+    case 'yes':
+    case 'y' : 
+    alert('you get that')
+break;
+    case 'no':
+    case 'n' : 
+    alert ('runge answer')
+break;
+default:
+    alert('invalid answer')
+}
+
+let Qus3= prompt('Do you think I\'m 24 yers old  ?') ;
+
+switch (Qus3.toLowerCase()) {
+
+    case 'yes':
+    case 'y' : 
+    alert('you get that')
+break;
+    case 'no':
+    case 'n' : 
+    alert ('runge answer')
+break;
+default:
+    alert('invalid answer')
+}
+
+let Qus4= prompt('Do you think i\'m an engineer ?');
+
+switch (Qus4.toLowerCase()) {
+
+    case 'yes':
+    case 'y' : 
+    alert('you get that')
+break;
+    case 'no':
+    case 'n' : 
+    alert ('runge answer')
+break;
+default:
+    alert('invalid answer')
+}
+
+let Qus5= prompt('Do you know where I live ?')
+
+switch (Qus5.toLowerCase()) {
+
+    case 'yes':
+    case 'y' : 
+    alert('you get that')
+break;
+    case 'no':
+    case 'n' : 
+    alert ('runge answer')
+break;
+default:
+    alert('invalid answer')
+}
+
+conf= confirm('Thank you '+ Qus+' ,you can know more about me here ') ;
