c5d9c38914856f9fa498f95426aadee1bd5fe9d4
Osamamomani1 <momaniosama97@gmail.com>
2021-04-27 15:45:19 +0300

    geuss game

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'user strict';
 
+let score=0 ;
+
 alert('welcome to Osama Momani page') ;
 
 let Qus= prompt('what\'s your name ?');
@@ -13,6 +15,7 @@ switch (Qus1.toLowerCase()) {
     case 'yes':
     case 'y' : 
     alert('you get that')
+    score++;
 break;
     case 'no':
     case 'n' : 
@@ -29,6 +32,7 @@ switch (Qus2.toLowerCase()) {
     case 'yes':
     case 'y' : 
     alert('you get that')
+    score++;
 break;
     case 'no':
     case 'n' : 
@@ -45,6 +49,7 @@ switch (Qus3.toLowerCase()) {
     case 'yes':
     case 'y' : 
     alert('you get that')
+    score++;
 break;
     case 'no':
     case 'n' : 
@@ -61,6 +66,7 @@ switch (Qus4.toLowerCase()) {
     case 'yes':
     case 'y' : 
     alert('you get that')
+    score++;
 break;
     case 'no':
     case 'n' : 
@@ -77,6 +83,7 @@ switch (Qus5.toLowerCase()) {
     case 'yes':
     case 'y' : 
     alert('you get that')
+    score++;
 break;
     case 'no':
     case 'n' : 
@@ -87,3 +94,57 @@ default:
 }
 
 conf= confirm('Thank you '+ Qus+' ,you can know more about me here ') ;
+
+let correct_answer=7;
+let i;
+
+//Qus6 = parseInt(Qus6)
+for (i = 0; i<4 ; i++) {
+    let Qus6 = prompt('what is my favorite number between (1-10) you have four opportunities');
+    Qus6 = parseInt(Qus6);
+    if (Qus6 !== correct_answer ) {
+        if (Qus6<correct_answer) {
+            alert('your answer is too low , please try again');
+            continue;
+        }
+        if (Qus6>correct_answer) {
+            alert('your answer is too high, please try again');
+            continue;
+
+        }
+    }
+    if (Qus6 === correct_answer){
+        alert('you answer is correct');
+        score++;
+        break;
+    }
+}
+if (i==4){
+    alert('you use all of your chances to guess my favorite number , the answer is 7');
+}
+
+
+let favAnime;
+favAnime = ['attack on titan','death note','hunterxhunter'];
+let value = false;
+
+for (let i=0 ; 1<6 ; i++){
+    let Qus7 = prompt('guess one of my favorite 3 anime, you have 6 attempts');
+    for (let j=0 ; j<3; j++){
+        if (Qus7.toLowerCase() === favAnime[j]){
+            value = true;
+            break;
+        }
+        
+    }
+    if (value==true){
+        alert('youe answer is correct');
+        score++;
+        break; 
+    }
+    else {
+        alert ('your answer is not correct ,please try again');
+    }
+}   
+
+alert('your score is '+score+' from 7')
